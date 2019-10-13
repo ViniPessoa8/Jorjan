b19877fe40ba5e4c3e508778e909f1b2ab848ab2
Jansen Junior <jsd.alcantara@gmail.com>
2019-10-13 14:03:19 -0400

    base login alg

--- src/bps/auth.py
+++ src/bps/auth.py
@@ -1,7 +1,17 @@
-from flask import Blueprint
+from ..util.jwt_manager import decode, encode
+from flask import Blueprint, request
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/login')
+@bp.route('/login', methods=['POST'])
 def login():
+    email = request.json['email']
+    ps    = request.json['password']
+
+    encoded = encode({'email': email, 'ps': ps})
+    print(encoded)
+
+    decoded = decode(encoded)
+    print(decoded)
+
     return 'login'
---
+++ src/util/jwt_manager.py
@@ -0,0 +1,10 @@
+from jwt import encode as cod, decode as dcod
+
+SECRET_KEY = 'UM_CODIGO_SUPER_SECRETO_PRA_NINNGUEM_HACKEAR_A_GENTE'
+ALGORITHM  = 'HS256'
+
+def encode(payload):
+    return cod(payload, SECRET_KEY, algorithm=ALGORITHM)
+
+def decode(encoded):
+    return dcod(encoded, SECRET_KEY, algorithms=[ALGORITHM])
