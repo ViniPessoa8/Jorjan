c901fc8fab044cec21f0d8d1a328e7e675b9ad73
Vinícius Pessoa <vini.pessoa7@gmail.com>
2019-11-02 10:18:17 -0400

    fix: user queries em inglês

--- src/db/user_queries.py
+++ src/db/user_queries.py
@@ -1,31 +1,31 @@
 def get_users():
-    return "SELECT * FROM usuario;"
+    return "SELECT * FROM user;"
 
 def get_user_info(auth):
-    return "SELECT nome, email FROM usuario WHERE auth='%s';" % (auth)
+    return "SELECT name, email FROM user WHERE auth='%s';" % (auth)
 
 def get_user_by_email(email):
-    return "SELECT nome, email, auth, id FROM usuario WHERE email='%s'" % (email)
+    return "SELECT name, email, auth, id FROM user WHERE email='%s'" % (email)
 
 def get_user_by_email_ps(email, ps):
-    return "SELECT nome, email, auth, id FROM usuario WHERE (email='%s' and senha='%s')" % (email, ps)
+    return "SELECT name, email, auth, id FROM user WHERE (email='%s' and password='%s')" % (email, ps)
 
 def get_user_by_auth(auth):
-    return "SELECT email FROM usuario WHERE auth='%s';" % auth
+    return "SELECT email FROM user WHERE auth='%s';" % auth
 
 def register_user(email, name, ps):
-    return "INSERT INTO usuario(nome, email, senha) \
+    return "INSERT INTO user(name, email, password) \
         VALUES ('%s', '%s', '%s');" % (name, email, ps)
 
 def register_user_with_auth(email, name, ps, auth):
-    return "INSERT INTO usuario(nome, email, senha, auth) \
+    return "INSERT INTO user(name, email, password, auth) \
         VALUES ('%s', '%s', '%s', '%s');" % (name, email, ps, auth)
 
 def remove_user(email, ps):
-    return "DELETE FROM usuario WHERE (email='%s' AND senha='%s')" % (email, ps)
+    return "DELETE FROM user WHERE (email='%s' AND password='%s')" % (email, ps)
     
 def update_auth(auth, email, ps):
-    return "UPDATE usuario SET auth = '%s' WHERE email = '%s' AND senha = '%s'" % (auth, email, ps)
+    return "UPDATE user SET auth = '%s' WHERE email = '%s' AND password = '%s'" % (auth, email, ps)
 
 def update_pass_by_id(email, new_pass):
-    return "UPDATE usuario SET senha='%s' WHERE email='%s';" % (new_pass, email)
+    return "UPDATE user SET password='%s' WHERE email='%s';" % (new_pass, email)
