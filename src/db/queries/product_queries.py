00b6d221db375bad549471fa113c3e5cedb98840
Jansen Junior <jsd.alcantara@gmail.com>
2019-11-06 23:54:13 -0400

    get products list

--- src/db/queries/product_queries.py
+++ src/db/queries/product_queries.py
@@ -1,3 +1,6 @@
 def qr_register_product(product, owner_id):
     return f"INSERT INTO product(name, description, category, price, stock, owner_id) \
         VALUES ('{product['name']}', '{product['description']}', {product['category_id']}, {product['price']}, {product['stock']}, {owner_id})"
+
+def qr_get_products():
+    return "SELECT * FROM product;"
