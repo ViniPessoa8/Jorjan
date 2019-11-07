c2832c7656fe7d08ed80544f2aebd125646580a2
Vinícius Pessoa <vini.pessoa7@gmail.com>
2019-11-07 13:28:08 -0400

    Update src/db/queries/category_queries.py
    Co-Authored-By: Jansen Saunier <jsd.alcantara@gmail.com>

--- src/db/queries/category_queries.py
+++ src/db/queries/category_queries.py
@@ -2,7 +2,7 @@ def qr_register_category(name):
     return "INSERT INTO Jorjan.category(`name`) VALUES (\""+name+"\");"
 
 def qr_get_category_by_id(id):
-    return "SELECT * FROM Jorjan.category WHERE id = "+id+";"
+    return f"SELECT * FROM Jorjan.category WHERE id = {id};"
 
 def qr_get_category_by_name(name):
     return "SELECT * FROM Jorjan.category WHERE name = \""+name+"\";"
