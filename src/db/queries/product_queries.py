0c887ce0f6f369781b0ae5c99ed4c001235948bd
Vinícius Pessoa <vini.pessoa7@gmail.com>
2019-12-02 21:18:57 -0400

    fix: get_product_seller query

--- src/db/queries/product_queries.py
+++ src/db/queries/product_queries.py
@@ -10,10 +10,9 @@ def qr_get_product_id(product_id):
     return f"SELECT * FROM product WHERE id={product_id};"
 
 def qr_get_products_seller(seller_id):
-    return f""" SELECT  FROM Jorjan.product p, Jorjan.`user` u, Jorjan.product prod, Jorjan.category c
-                WHERE p.owner_id={seller_id} AND u.id={seller_id} AND prod.owner_id = {seller_id}
-                   AND c.id = prod.category
-                GROUP BY u.name;"""
+    return f"""SELECT * FROM Jorjan.product p, Jorjan.`user` u, Jorjan.product prod, Jorjan.category c
+            WHERE p.owner_id={seller_id} AND u.id={seller_id} AND prod.owner_id = {seller_id}
+            AND c.id = prod.category;"""
 
 
 def qr_update_product_stock(product_id, quantity):
