7896f20ca6f4ef2630c6f49588664683f34ff3ea
Vinícius Pessoa <vini.pessoa7@gmail.com>
2019-12-02 21:17:30 -0400

    query get_products_seller

--- src/db/queries/product_queries.py
+++ src/db/queries/product_queries.py
@@ -3,13 +3,18 @@ def qr_register_product(product, owner_id):
         VALUES ('{product['name']}', '{product['description']}', {product['category_id']}, {product['price']}, {product['stock']}, {owner_id})"
 
 def qr_get_products():
+    print("SELECT * FROM product;")
     return "SELECT * FROM product;"
 
 def qr_get_product_id(product_id):
     return f"SELECT * FROM product WHERE id={product_id};"
 
 def qr_get_products_seller(seller_id):
-    return f"SELECT * FROM product WHERE owner_id={seller_id};"
+    return f""" SELECT  FROM Jorjan.product p, Jorjan.`user` u, Jorjan.product prod, Jorjan.category c
+                WHERE p.owner_id={seller_id} AND u.id={seller_id} AND prod.owner_id = {seller_id}
+                   AND c.id = prod.category
+                GROUP BY u.name;"""
+
 
 def qr_update_product_stock(product_id, quantity):
     return f"UPDATE product SET stock={quantity} WHERE id={product_id}"
